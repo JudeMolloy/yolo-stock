4109f55859e4868b12f0adc38eaed2d73b02036c
Gabriel Jones <gabrieljaredjones@gmail.com>
2021-03-07 05:30:44 +0000

    top tickers

--- commands/update_tickers.py
+++ commands/update_tickers.py
@@ -1,28 +1,12 @@
 from app import db
 from flask_script import Command
-from bs4 import BeautifulSoup
 import requests
+import random
 
 
 class UpdateTickersCommand(Command):
 	def run(self):
-		res = requests.get(TICKER_URL)
+		res = requests.get("https://api.swaggystocks.com/wsb/sentiment/top?limit=500")
+		tickers = res.json()[:100]
 
-		html_raw = res.content.decode("utf-8")
-		html = BeautifulSoup(html_raw, 'html.parser')
-
-		containers = html.body.find_all("div", class_="volume-ticker-entry")
-		if len(containers) == 0:
-			print("Can't find root element")
-			return
-
-		tickers = []
-
-		for container in containers:
-			names = html.body.find_all("div", class_="volume-ticker-name")
-			
-			if len(names) == 0: continue
-
-			tickers.append(names[0])
-
-		print(tickers)
+		print(random.choices(tickers, k=3))
